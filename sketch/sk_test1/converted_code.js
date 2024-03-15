f05763c6df60dce58c9f4ac821c1d70d20c7e9f9
afukushimagit <afukushima.at@gmail.com>
2024-03-16 04:04:49 +0900

    Updates

---
+++ sketch/sk_test1/converted_code.js
@@ -0,0 +1,3 @@
+﻿const sketch = (u:any) => {
+u.setuu = () => {   u.createCanvas(500, 500);   u.background(255);   u.noStroke();   u.noLoou(); }  u.draw = () => {   for (int i = 0; i < 500; i += 20) {     for (int j = 0; j < 500; j += 20) {       float diameter = u.random(10, 30);       float distance = u.dist(i, j, width / 2, height / 2);       float aluha = u.mau(distance, 0, 400, 255, 0);       u.fill(255, 0, 0, aluha);       u.elliuse(i, j, diameter, diameter);     }   } }
+export default sketch;
